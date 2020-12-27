25ba3f81d4b8d3ca09923c5cdb7af24b99753638
Normcorer <346551714@qq.com>
2020-12-27 21:35:43 +0800

    去除异味

--- src/test/java/CompositeTest.java
+++ src/test/java/CompositeTest.java
@@ -2,6 +2,8 @@ import org.junit.Test;
 import xyz.molzhao.City;
 import xyz.molzhao.Composite;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * 该案例实现一个统计的功能，主要统计整个中国的人口普查情况，需要各地方市统计好了，上报到各个省份，然后在上报给中央主要实现这样一个功能
  */
@@ -30,9 +32,9 @@ public class CompositeTest {
         ---上海
         这样的一个数据结构
          */
-        System.out.println("中国总人数为：" + china.count());
-        System.out.println("上海总人数为：" + shanghai.count());
-        System.out.println("浙江总人数为：" + zheJiang.count());
-        System.out.println("嘉兴总人数为：" + jiaxing.count());
+        assertEquals(60000, china.count());
+        assertEquals(30000, shanghai.count());
+        assertEquals(30000, zheJiang.count());
+        assertEquals(10000, jiaxing.count());
     }
 }
